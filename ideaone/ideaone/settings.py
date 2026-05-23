29393e3047930200d6b208870750abb4d19e4072
defnotamirr <amirertaev22@gmail.com>
2026-05-23 14:25:42 +0500

    fix looking/speaking images

--- ideaone/ideaone/settings.py
+++ ideaone/ideaone/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://djangodiary-production-0d15.up.railway.app']
